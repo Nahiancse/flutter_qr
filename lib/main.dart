c101d53205b06b7b18fb30117b659c8d4e6f4d1d
nahian <cnahian4@gmail.com>
2022-06-08 10:07:03 +0600

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -34,15 +34,202 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title!),
       ),
-      body: Center(
-        child: ElevatedButton(
-            child: Text('Scan'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Scanner()),
-              );
-            }),
+      body: Container(
+        margin: EdgeInsets.only(top: 100),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(right: 5),
+                    height: 80,
+                    width: 150,
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Total Data',
+                          style: TextStyle(
+                              color: Color(0xff010133),
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '50',
+                          style: TextStyle(
+                              color: Color(0xff010133),
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    height: 80,
+                    width: 150,
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Saved Data',
+                          style: TextStyle(
+                              color: Color(0xff010133),
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '10',
+                          style: TextStyle(
+                              color: Color(0xff010133),
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(right: 5),
+                    height: 80,
+                    width: 150,
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Rejected Data',
+                          style: TextStyle(
+                              color: Color(0xff010133),
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '5',
+                          style: TextStyle(
+                              color: Color(0xff010133),
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    height: 80,
+                    width: 150,
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        Text(
+                          'My Profile',
+                          style: TextStyle(
+                              color: Color(0xff010133),
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Scanner()),
+                      );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 5),
+                      height: 50,
+                      width: 250,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: Text(
+                        'Scan',
+                        style: TextStyle(
+                            color: Color(0xff010133),
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    height: 50,
+                    width: 250,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Text(
+                      'Saved List',
+                      style: TextStyle(
+                          color: Color(0xff010133),
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    height: 50,
+                    width: 250,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Text(
+                      'Rejected List',
+                      style: TextStyle(
+                          color: Color(0xff010133),
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    height: 50,
+                    width: 250,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Text(
+                      'Logout',
+                      style: TextStyle(
+                          color: Color(0xff010133),
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
